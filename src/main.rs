37268ec19542811ff96e32b8b3d17ab2ab4a3538
Will Pragnell <wpragnell@gmail.com>
2021-11-18 16:15:22 -0800

    add filter text box & play button

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ const WHITE: egui::Color32 = egui::Color32::from_rgb(255, 255, 255);
 
 struct Erabu {
     projects: Vec<Project>,
+    filter: String,
+}
+
+struct Project {
+    title: String,
+    tags: Vec<String>,
 }
 
 impl Erabu {
@@ -22,6 +28,7 @@ impl Erabu {
         });
         Erabu {
             projects: Vec::from_iter(iter),
+            filter: "".to_owned(),
         }
     }
 
@@ -44,11 +51,45 @@ impl Erabu {
         });
         ui.add_space(PADDING);
     }
-}
 
-struct Project {
-    title: String,
-    tags: Vec<String>,
+    fn render_controls(&mut self, ui: &mut Ui) {
+        ui.add_space(PADDING);
+        ui.add(egui::TextEdit::singleline(&mut self.filter).desired_width(f32::INFINITY));
+        ui.add_space(PADDING);
+        ui.vertical_centered(|ui| {
+            self.draw_play_icon(ui);
+        });
+        ui.add_space(PADDING);
+    }
+
+    fn draw_play_icon(&self, ui: &mut Ui) {
+        let size = egui::Vec2::splat(18.0);
+        let stroke = egui::Stroke::new(2.0, WHITE);
+        let (response, painter) = ui.allocate_painter(size, egui::Sense::click());
+        let rect = response.rect;
+
+        let (c, h, w) = (rect.center(), rect.height(), rect.width());
+        let top_left = egui::Pos2 {
+            x: c.x - w / 2.0 + 2.0,
+            y: c.y - h / 2.0,
+        };
+        let bottom_left = egui::Pos2 {
+            x: c.x - w / 2.0 + 2.0,
+            y: c.y + h / 2.0,
+        };
+        let right_above = egui::Pos2 {
+            x: c.x + w / 2.0 - 2.0,
+            y: c.y - 0.5,
+        };
+        let right_below = egui::Pos2 {
+            x: c.x + w / 2.0 - 2.0,
+            y: c.y + 0.5,
+        };
+
+        painter.line_segment([top_left, bottom_left], stroke);
+        painter.line_segment([top_left, right_below], stroke);
+        painter.line_segment([bottom_left, right_above], stroke);
+    }
 }
 
 impl epi::App for Erabu {
@@ -57,6 +98,10 @@ impl epi::App for Erabu {
     }
 
     fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
+        egui::TopBottomPanel::bottom("controls").show(ctx, |ui| {
+            self.render_controls(ui);
+        });
+
         egui::CentralPanel::default().show(ctx, |ui| {
             // this actually creates a very small gap at the top for release
             // builds, but let's not worry about that for now!
